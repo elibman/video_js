72e8cbb33e820967c5ba55f2cc5cefb6607b9675
elibman <123482125+elibman@users.noreply.github.com>
2023-11-06 15:26:36 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,30 @@
-var video;
+var video = document.querySelector('#player1');
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
+	//initialize
+	video.autoplay = false;
+	video.pause();
+	video.loop = fasle;
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+//play button + volume
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	
+});
+//pause button
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+	
+});
 
+//slow down
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	video.slow();
+	
+});
